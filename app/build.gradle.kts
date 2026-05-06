96828b4100409cb05ec131f2af9ea835fd6211cd
yx <1003241377@qq.com>
2026-05-06 22:12:06 +0800

    升级版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ android {
         applicationId = "com.example.ttai"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
